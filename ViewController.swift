6a7f4c8ba2c27423bbfc38b693a6b8eb63dedb62
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-01-25 19:55:19 -0500

    boss challenge completed

---
+++ ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import  AVFoundation
+
+class ViewController: UIViewController {
+  
+    var audioPlayer: AVAudioPlayer?
+    var currentTitle : String?
+    
+   
+    @IBAction func audioKeyPressed(_ sender: UIButton) {
+        print(sender)
+        print(sender.backgroundColor)
+        print(sender.currentTitle);
+        var sound = (sender.currentTitle)!
+        sender.alpha = 0.5
+        print("start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+            print("End")
+        };
+        print("Button got pressed")
+        playSoundC(soundName: sound)
+    }
+    // Xylophone
+
+    func playSoundC(soundName:String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: url!  )
+            audioPlayer?.play()
+                    
+        }
+}
+    
+        
+    //*********
+    
+    //*********
+    /*func playSoundC() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = audioPlayer else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }*/
+   
+
+    
+    
+    
+
+
+
+    
+
